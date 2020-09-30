8fe5f6ed54399506185f328ce2cd9717d312d49c
sscaggs13 <s96scaggs@gmail.com>
2020-09-29 22:22:39 -0500

    Building Framework for animation
    Just the beginnings of the animation framework. Trying to workout how to setup the animation dynamically so that the animation and sprites can be set depending on the character that they are playing.

---
+++ Assets/Scripts/Player/PlayerAnimationController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.U2D.Animation;
+
+public class PlayerAnimationController : NetworkBehaviour
+{
+    [SerializeField] private Sprite _playerSprite = null;
+    [SerializeField] private Animator _animator = null;
+
+    public void SetPlayerSprite(Sprite pSprite)
+    {
+        _playerSprite = pSprite;
+        GetComponentInChildren<SpriteRenderer>().sprite = _playerSprite;
+        GetComponentInChildren<SpriteSkin>();
+    }
+}
