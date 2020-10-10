43fa02d6ca16251cc3025107316224c5b78863bd
Jacob <jacobscaggs98@gmail.com>
2020-10-10 16:37:59 -0500

    Added .psd Sprite and Bones to Other Sprite In Jacob's Sandbox
    I added to Jacob's Sandbox and I added diferent Sprites to it and added bones to the split naked man sprite for animation purposes. Also coded a simple script for changing hair but haven't put it to use yet.

---
+++ Assets/Sandbox/Jacob/Scripts/Customize_Options.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Customize_Options : MonoBehaviour
+{
+    [Header("Sprite To Change")]
+    public SpriteRenderer bodyPart;
+
+    [Header("Sprites To Cycle Through")]
+    public List<Sprite> options = new List<Sprite>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
