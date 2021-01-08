07c68d2fad5341914205966c84068e1e4e1de389
sscaggs13 <s96scaggs@gmail.com>
2021-01-07 19:35:20 -0600

    Added new tiles
    Added the tile set and set up for placing the tiles.

--- Assets/Scripts/ScriptableObjects/Craftables/CraftingRecipe.cs
+++ Assets/Scripts/ScriptableObjects/Craftables/CraftingRecipe.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using static Constants;
 
 public struct MaterialAmount
 {
-    public UnityEngine.Material material;
+    public Material material;
+    [Range(1,999)]
     public int amount;
 }
 
 public struct EquipmentAmount
 {
+    public IItem item; // TODO: May work on changing this to an ICraftable Type instead. 
+    [Range(1,999)]
+    public int amount;
 }
 
 public class CraftingRecipe : ScriptableObject
 {
     public List<MaterialAmount> Materials;
     public List<EquipmentAmount> Results;
+
+    public bool CanCraft(Dictionary<ITEMS, int> pInventory)
+    {
+        bool canCraft = true;
+        for (int i = 0; i < Materials.Count; i++)
+        {
+            if(pInventory[Materials[i].material.GetMaterialType()] < Materials[i].amount)
+            {
+                canCraft = false;
+            }
+        }
+
+        return canCraft;
+    }
 }
